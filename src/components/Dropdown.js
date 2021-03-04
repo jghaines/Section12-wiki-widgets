45b8750e18e41d3a199c73b05d519ba36d154561
Jason Haines <github.com@jhaines.name>
2021-03-05 09:35:06 +1100

    open/close dropdown #178

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 const Dropdown = ({ prompt, options, selected, onSelectedChange }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     const renderedOptions = options.map( option => {
         if (option.value === selected.value) {
             return null;
@@ -17,10 +21,13 @@ const Dropdown = ({ prompt, options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">{prompt}</label>
-                <div className="ui selection dropdown visible active">
+                <div
+                    onClick={ () => setIsOpen(!isOpen) }
+                    className={`ui selection dropdown ${ isOpen ? 'visible active': '' }`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${ isOpen ? 'visible transition': '' }`}>
                         {renderedOptions}
                     </div>
                 </div>
